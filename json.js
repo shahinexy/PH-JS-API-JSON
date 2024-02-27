c7b0e63295f542de8eed5bc836a9967453d09fcb
shahinexy <shahinexy@gmail.com>
2024-02-27 22:33:56 +0600

    more jeson details added

--- json.js
+++ json.js
@@ -7,9 +7,27 @@ function data() {
 const ul = document.getElementById('person');
 function personData(datas){
     for(const data of datas){
-        console.log(data.username);
         const li = document.createElement('li');
         li.innerText = data.username;
         ul.appendChild(li)
+    }
+}
+
+function post() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => postData(json))
+}
+
+function postData(posts){
+    const divContaier = document.getElementById('post_container');
+    for(const post of posts){
+        const div = document.createElement('div')
+        div.innerHTML =    `
+        <h1>Person ID: ${post.id}</h1>
+        <h2>Post Title: ${post.title}</h2>
+        <p>Post Description: ${post.body}</p>
+        `
+        divContaier.appendChild(div);
     }
 }
